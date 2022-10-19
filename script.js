dbb73d7da23fcced1b2b293fc33c38510fdef26b
Edilberto Cantuaria <edilbertocantuaria@users.noreply.github.com>
2022-10-19 14:17:42 -0300

    Loop inicial
    Função implementada: ao entrar na página, aparece o prompt perguntando o número de cartas que só para quando
    - numCarta número par e
    (numCartas>=4 &&numCartas<=14)
    
    Também sai do looping quando o clica em "cancelar" no prompt

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-prompt("Olá, vamos jogar um jogo! 😁");
-console.log("funcionando");
+let boraJogar=0;
+function inciarJogo(){
+do{
+let numCartas= prompt("Olá, vamos jogar um jogo! 😁 \nDigite um número par entre 4 e 14");
+
+/*Esse if foi colocado exatamente para sair quando o usuário clicar em "cancelar" no console*/
+if (numCartas===null ){
+   boraJogar=1
+} 
+else {
+numCartas = Number(numCartas);
+const numImpar = numCartas%2;
+
+if ((numCartas>=4 &&numCartas<=14) && (numImpar==0)){
+    boraJogar=1;
+}
+} 
+}while  (boraJogar!=1)
+}
