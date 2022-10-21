b09829683ef9baa78c317b1028f4ac864b7a7bc4
Edilberto Cantuaria <edilbertocantuaria@users.noreply.github.com>
2022-10-21 09:48:58 -0300

    Ícone arrumado
    tava fazendo do jeito difícil

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let boraJogar=0;
 let numCartas= prompt("Olá, vamos jogar um jogo! 😁 \nDigite um número par entre 4 e 14");
 
+const parrots=[
+  'bobrossparrot.gif',
+  'explodyparrot.gif',
+  'fiestaparrot.gif',
+  'metalparrot.gif',
+  'revertitparrot.gif',
+  'tripletsparrot.gif',
+  'unicornparrot.gif'
+];
+
+const numCartasSelecionadas=[]; 
+
+
+
+
 do{
 /*Esse if foi colocado exatamente para sair quando o usuário clicar em "cancelar"*/
 if (numCartas===null ){
@@ -12,7 +27,14 @@ const numImpar = numCartas%2;
 
 if ((numCartas>=4 &&numCartas<=14) && (numImpar==0)){
     boraJogar=1;
-  adicionarCartas(numCartas)
+
+  let widthJogo = (numCartas)/2 *150
+  let dim = document.querySelector(".cartas");
+  dim.style.width = widthJogo + "px"
+   
+  adicionarCartas(numCartas);
+  colocarVerso(numCartas);
+
 }
 else{
 numCartas= prompt("Olá, vamos jogar um jogo! 😁 \nDigite um número par entre 4 e 14");
@@ -24,36 +46,46 @@ function adicionarCartas(numCartas){
     const adicionarCartas = document.querySelector('.cartas');
 
 for(let contador=0;contador<numCartas; contador++){
-adicionarCartas.innerHTML += `<div class="carta centHorVert" onclick="virarCarta(this, '.papagaioVerso')"> 
-<div class="versoCarta">
-<img class="papagaioVerso"src="Imagens/Para as cartas/back.png" class="MostrarParrot"/>
+adicionarCartas.innerHTML += `<div class="carta" onclick="virarCarta(this)"> 
+<div class="face frenteCarta centHorVert">
+<img class="papagaioFrente" src="Imagens/Para as cartas/tripletsparrot.gif" class="MostrarParrot"/>
+</div>
+
+<div class="face versoCarta centHorVert">
+<img class="papagaioVerso" src="Imagens/Para as cartas/back.png" class="MostrarParrot"/>
+</div>
+
 </div>`
 } 
 }
 
-function virarCarta(cartaSelecionada,  imgPapagaio){
-  const imgPapagaioVirado =document.querySelector('.virar .versoCarta .papagaioVerso');
-  
-  imgPapagaio = document.querySelector('.versoCarta .papagaioVerso');
-  //  imgPapagaio = document.querySelector('.versoCarta .papagaioVerso');
-
-  if (imgPapagaioVirado===null){
-  imgPapagaio.classList.add("ocultarCarta")
-  alert("entrou no if")
-  console.log(imgPapagaio)
-  } else { 
-    imgPapagaio.classList.remove("ocultarCarta")
-   alert("entrou no else")
-    console.log(imgPapagaio)
-  }
+function virarCarta(cartaSelecionada){
  cartaSelecionada.classList.toggle("virar")
+}
+
+
+// function virarCarta(cartaSelecionada,  imgPapagaio){
+// const imgPapagaioVirado =document.querySelector('.virar .versoCarta .papagaioVerso');
+// imgPapagaio = document.querySelector('.versoCarta .papagaioVerso');
+// //  imgPapagaio = document.querySelector('.versoCarta .papagaioVerso');
+
+// if (imgPapagaioVirado===null){
+// imgPapagaio.classList.add("ocultarCarta")
+// alert("entrou no if")
+// console.log(imgPapagaio)
+// } else { 
+//   imgPapagaio.classList.remove("ocultarCarta")
+//  alert("entrou no else")
+//   console.log(imgPapagaio)
+// }
+// cartaSelecionada.classList.toggle("virar")
 
 //  const ocultarCartaSelecionada = document.querySelector (".carta .versoCarta .virar")
 // console.log (ocultarCartaSelecionada)
 
 // cartaSelecionada.classList.toggle("versoCarta ocultarCarta")
 
-}
+// }
 
 // do{
 // const parada = prompt(`Parabéns! Você encerrou o jogo em XX:XX minutos e com XX jogadas! 
@@ -62,3 +94,16 @@ function virarCarta(cartaSelecionada,  imgPapagaio){
 // console.log(parada)
 
 // }while (parada==="sim")
+
+// function compararCartas(){
+// //pega as cartas viradas e compara se elas são iguais
+// //se são iguais, não vai virar novamente
+// //se são  diferentes, vai desvirar dentro de 1s  
+// // setTimeout (desvirarCartas, 1000)  -> SEM PARÊNTESES NO PARÂMETRO!
+// }
+
+
+//setInterval(iniciarJogo, 1s)  //contador de tempo do bônus, ele vai liberar um códigozinho no console, vamos utilizar esse código para parar
+//const codigo = setInterval(iniciarJogo, 1s) 
+//clearInterval(codigo) // para parar a contagem do tempo
+
